fef750aa664f7c71874a36e86cd11b26bd8e2f5b
Iurioas <iurioas.adv@gmail.com>
2025-02-26 23:08:15 -0300

    Update sounds.js

--- sounds.js
+++ sounds.js
@@ -1,9 +1,8 @@
 export const playClickSound = () => {
   try {
-    console.log("Tentando tocar som de clique...");
     const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-modern-click-box-check-1120.mp3');
     audio.volume = 0.3;
-    audio.play().then(() => console.log("Som de clique tocado com sucesso!")).catch(error => console.error("Erro ao tocar som de clique:", error));
+    audio.play().catch(error => console.error("Erro ao tocar som de clique:", error));
   } catch (error) {
     console.error("Erro ao carregar som de clique:", error);
   }
@@ -21,5 +20,5 @@ export const playSuccessSound = () => {
 
 // Pré-carrega sons após a primeira interação
 document.addEventListener("click", () => {
-  playClickSound(); // Toca uma vez para liberar a política de autoplay
+  playClickSound();
 }, { once: true });
